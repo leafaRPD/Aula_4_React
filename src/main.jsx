12a12a83226092c9dab0d836515073b2b31e9ce2
RAFAEL PAZETTO DAMASIO <rafael_p_damasio@estudante.sesisenai.org.br>
2023-11-27 22:02:48 -0300

    Atualização da 22h

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 
 import GlobalStyles from './styles/global'
 import {ThemeProvider} from 'styled-components'
+
 import theme from './styles/theme'
 
-import {Home} from './pages/Home'
-import {Destiny} from './pages/Destiny'
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home/>
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
